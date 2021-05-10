89bee58ecf8987a434bc15582e1807da6ba244df
Aditya1405 <adityaprakash0598@gmail.com>
2021-05-10 16:40:56 +0530

    commit 6
    signin has been created

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -14,13 +14,28 @@ router.post('/signup', async (req, res) => {
         const user = new User({ phone });
         //here data validation takes place
         await user.save();
-
+        //signing key of jwt = 'HS256'
         const token = jwt.sign({ userId: user._id }, 'HS256');
         res.send({ token: token });
     } catch (err) {
         //error handling for user inputs
         return res.status(422).send(err.message);
     }
+});
+
+router.post('/signin', async (req, res) => {
+    //when user signs in we expect a phone num from the user input
+    const { phone } = req.body;
+    if (!phone) {
+        return res.status(422).send({ error: "Must provide phone num " })
+    }
+    //we need to pick a user from the supplied phone
+    const user = await User.findOne({ phone: phone });
+    if (!user) {
+        return res.status(404).send({ error: 'phone not found' });
+    }
+    const token = jwt.sign({ userId: user._id }, 'HS256');
+    res.send({ token });
 });
 
 module.exports = router;
